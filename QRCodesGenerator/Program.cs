58a966b734197f985102d216ce16dd2e370c8f92
dimasickx <dmitry.sorokin.mail@gmail.com>
2021-04-24 15:20:51 +0500

    Initial commit

---
+++ QRCodesGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QRCodesGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
