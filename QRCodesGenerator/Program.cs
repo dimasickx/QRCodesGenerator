ce69cd5a68d48e0eff14d4a69d036dbb5e1b2654
dimasickx <dmitry.sorokin.mail@gmail.com>
2021-04-27 22:11:55 +0500

    end split data to blocks

--- QRCodesGenerator/Program.cs
+++ QRCodesGenerator/Program.cs
@@ -4,21 +4,24 @@ using System.Text;
 namespace QRCodesGenerator
 {
     public delegate string Encode(string data);
+
     class Program
     {
         static void Main()
         {
-            const string example = "1101000010100101110100001011000011010000101100011101000110000000";
-            Console.WriteLine(example.Length);
             var encode = new Encode(TypeEncoding.ByteEncode);
-            var data = new Data("Хабр", CorrectionLevel.M, encode, "0100");
-            var resultData = QrCodeInfo.AddServiceInfo(data);
-            var result = resultData.ComplementUpToMultiple8();
-            var fullResult = result.ComplementUpToVersion(data);
-            data.DataBit = fullResult;
-            Console.WriteLine(resultData);
-            Console.WriteLine(result);
-            Console.WriteLine(fullResult);
+            
+            var data = new Data("Хабрахабраледядя че то поменяется?", CorrectionLevel.M, encode, "0100");
+            data.DataBit = QrCodeInfo
+                .AddServiceInfo(data)                               // мб сделать Extension
+                .ComplementUpToMultiple8()
+                .ComplementUpToVersion(data);
+            
+            var listOfBlocks = DataBlocks.SplitData(data);
+            foreach (var block in listOfBlocks)
+            {
+                Console.WriteLine(block.Data);
+            }
         }
     }
 }
--- QRCodesGenerator/QrCode.cs
+++ QRCodesGenerator/QrCode.cs
@@ -1,35 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace QRCodesGenerator
 {
     public class QrCode
     {
-        public string Data { get; set; }
-        public readonly CorrectionLevel Level;
-        public readonly Encode Encode;
-        public readonly string CodingType;
-        public int Version => GetVersion(Level, Data.Length);
-
-        public QrCode(CorrectionLevel level, Encode encode, string type)
-        {
-            Level = level;
-            Encode = encode;
-            CodingType = type;
-        }
-
-        private static int GetVersion(CorrectionLevel level, int dataLenght)
-        {
-            foreach (var l in TableOfVersions.VersionMap.Keys)
-            {
-                if (level != l) continue;
-                for (var i = 0; i < TableOfVersions.VersionMap[l].Length; i++)
-                {
-                    if (dataLenght < TableOfVersions.VersionMap[l][i])
-                        return i + 1;
-                }
-            }
-
-            throw new NullReferenceException("level not exist");
-        }
+        public List<Block> Blocks;
     }
 }
