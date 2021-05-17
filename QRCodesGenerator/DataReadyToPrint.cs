285d57cdfec0026f4b33b91f750079967ac8a45e
dimasickx <dmitry.sorokin.mail@gmail.com>
2021-05-17 18:50:00 +0500

    mini refactoring

---
+++ QRCodesGenerator/DataReadyToPrint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace QRCodesGenerator
+{
+    public static class DataReadyToPrint
+    {
+        public static string CombinationBlocks(List<DataBlock> dataBlocks, List<CorrectionBlock> correctionBlocks)
+        {
+            // var result = new int[dataBlocks.Count * 2];
+            // for (int i = 0; i < dataBlocks.Count; i++)
+            // {
+            //     for (int j = 0; j < dataBlocks[i].Data.Count; j++)
+            //     {
+            //         result[] = dataBlocks[i].Data[j]; 
+            //     }
+            // }
+            return "";
+        }
+    }
+}
